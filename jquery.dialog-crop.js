7b6c2968ddf16dd77f7ac8014b9d1ed084a523ce
Pedro Marcelo <pedromarcelodesaalves@gmail.com>
2015-04-29 11:51:18 -0300

    Versão 1.0 do plugin

--- jquery.dialog-crop.js
+++ jquery.dialog-crop.js
@@ -1,38 +1,78 @@
 /*!
-*	jQuery Dialog Crop v0.1 released under MIT License
+*	jQuery Dialog Crop v1.0 released under MIT License
 *	Author: Pedro Marcelo <pedromarcelodesaalves@gmail.com>
 *	https://github.com/pedromarcelojava/jQuery-Dialog-Crop
 */
 
 (function($){
 	$.initDialogCropOptions = function(options) {
-		if (typeof options === "undefined") options = {};
-		// Dialog
-		if (typeof options.dialog === "undefined") options.dialog = {};
-		if (typeof options.dialog.close === "undefined") options.dialog.close = function(evn, ui){ $(this).addClass("dialog-hide").dialog("destroy").html(""); };
-		options.dialog.position = {my: "center", at: "center", of: window};
-		// Jcrop
-		if (typeof options.jcrop === "undefined") options.jcrop = {};
+        var defaults = {
+            classHide : "dialog-hide",
+            imageClass : "",
+            dialog : {
+                position : {my: "center", at: "center", of: window},
+                close : function(evn, ui){
+                    $(this).addClass(options.classHide).dialog("destroy").html("");
+                }
+            },
+            jcrop : {}
+        };
 
-		return options;
+		return $.extend({}, defaults, options);
 	}
 
 	$.fn.dialogCrop = function(options) {
 		if (this.length > 0)
 		{
+            $.dialogCrop = {};
 			options = $.initDialogCropOptions(options);
 			var data = {
 				dialog: this,
 			};
 
+			$.widget("ui.dialog", $.extend({}, $.ui.dialog.prototype, {
+                _title: function(title) {
+                    var $title = this.options.title || '&nbsp;';
+                    if( ("title_html" in this.options) && this.options.title_html == true )
+                        title.html($title);
+                    else
+                        title.text($title);
+                }
+            }));
+
 			$("a[data-toggle=\"dialog-crop\"]").click(data, function(evn){
 				evn.preventDefault();
+                var img = new Image();
+                img.onload = function(){
+                    $.dialogCrop.originalWidth = this.width;
+                    $.dialogCrop.prop = $.dialogCrop.originalWidth / $.dialogCrop.dialogImageWidth;
+                    if (typeof options.formTarget !== "undefined")
+                    {
+                        var $jdcp = $('#jquery-dialog-crop-prop');
+
+                        if ($jdcp.length > 0)
+                        {
+                            $jdcp.val($.dialogCrop.prop);
+                        }
+                        else
+                        {
+                            $('<input>',{
+                                type: 'hidden',
+                                name: 'jquery-dialog-crop-prop',
+                                value: $.dialogCrop.prop
+                            }).appendTo(options.formTarget);
+                        }
+                    }
+                }
+                img.src = $(this).attr('href');
 				$('<img>', {
 					id: 'dialog-crop-image',
-					src: $(this).attr('href')
+					src: $(this).attr('href'),
+                    "class": options.imageClass
 				}).appendTo(evn.data.dialog);
-				$(evn.data.dialog).removeClass("dialog-hide").dialog(options.dialog);
+				$(evn.data.dialog).removeClass(options.classHide).dialog(options.dialog);
 				$(function(){$("#dialog-crop-image").Jcrop(options.jcrop);});
+                $.dialogCrop.dialogImageWidth = $('#dialog-crop-image').width();
 			});
 
 			$(window).resize(data, function(evn){
